372272042034074d2f28a0862f325b5ad756e037
monoter-1 <lpmontero@proton.me>
2023-03-09 21:28:35 +0100

    test commit

---
+++ index.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Leandro Montero's Homepage</title>
+<meta charset="utf-8">
+<link type="text/css" rel="stylesheet" href="styles/style.css" />
+<meta name="robots" content="index, follow, all" >
+<!--[if IE 6]><link rel="stylesheet" href="styles/ie6.css" /><![endif]-->
+<!--[if IE 7]><link rel="stylesheet" href="styles/ie7.css" /><![endif]-->
+</head>
+<body>
+<div id="wrap">
+  <div id="header">
+    <h1>Leandro Montero</h1>
+    <h2>Assistant Professor</h2>
+  </div>
+  <div id="bio">
+    <div id="about">
+      <div id="photo-bg"> <img src="images/foto4.jpg" alt="" width="180" height="240" /> </div>
+      <h3>About Me</h3>
+      <p>Assistant Professor - École d'Ingénieurs - ISEN Nantes.<br />
+	PhD - Université Paris-Sud.<br />
+	MSc Computer Science - Universidad de Buenos Aires. </p>
+      <h3>Research interests</h4>
+	 <p>Graph theory, algorithms, complexity and theoretical computer science.</p>
+    </div>
+    <div id="contact-details">
+      <h3>Contact Me</h3>
+      <p></p>
+<!--  <h5>Phone: <span>+352 46 66 44 5277</span></h5> -->
+      <h5>Email: <span>firstname.lastname [at] isen-ouest.yncrea.fr</span></h5>
+<!--  <h5><a href="http://wwwen.uni.lu/snt/research/sedan" target="_blank">SEDAN Research Group</a></h5> -->
+<!--
+<h5>Mailing address:</h5>
+<h5><span>Leandro Montero</span></h5>
+<h5><span>Université Paris-Sud, Orsay</span></h5>
+<h5><span>LRI, Bât. 650,</span></h5>
+<h5><span>91405 Orsay cedex</span></h5>
+<h5><span>FRANCE</span></h5> 
+-->
+    </div>
+  </div>
+  <div class="line"></div>
+<!--
+  <div id="featured-projects">
+    <h3>Featured Projects</h3>
+    <div class="project"> <a href="#"><img src="images/jengo440x240.jpg" alt="" /></a> </div>
+    <div class="project last"> <a href="#"><img src="images/soko440x240.jpg" alt="" /></a> </div>
+    <div class="project"> <a href="#"><img src="images/sanda440x240.jpg" alt="" /></a> </div>
+    <div class="project last"> <a href="#"><img src="images/kondo440x240.jpg" alt="" /></a> </div>
+  </div>
+  <div class="line"></div>
+-->
+  <div id="services">
+    <h3>Studies</h3>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>PhD - Université Paris-Sud, France - 2012.</h4>
+	<p>Thesis: "Graph and colors: Edge-colored graphs, edge-colorings and proper connections". Advisor: Prof. Yannis Manoussakis. 
+	Available <a href="http://tel.archives-ouvertes.fr/docs/00/77/68/99/PDF/VD2_MONTERO_LEANDRO_13122012.pdf" target="_blank">HERE</a>!</p>
+    </div>
+  </div>
+  <div id="services">
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>MSc Computer Science - Universidad de Buenos Aires, Argentina - 2008.</h4>
+	<p>Thesis: "Convergence and divergence of the iterated biclique graph". Advisor: Prof. Marina Groshaus. 
+	Available in spanish <a href="http://gestion.dc.uba.ar/media/academic/grade/thesis/lmontero.pdf" target="_blank">HERE</a>!</p>
+    </div>
+  </div>
+
+  <div class="line"></div>
+
+  <div id="services">
+    <h3>Teaching</h3>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>École d'Ingénieurs - ISEN Nantes (2018-present)</h4>
+	<p>Algorithms and programming in C; PHP; JavaScript; Graph theory, algorithms and complexity.</p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>Université Paris-Sud (2010-2016)</h4>
+	<p>Graph theory; Algorithms, data structures and complexity; Operational research; C, C++ & Java programming.</p>
+    </div>
+  </div>
+  <div id="services">
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>Universidad de Buenos Aires (2004-2009)</h4>
+	<p>Mathematics; Calculus; Graph theory; Algorithms, data structures and complexity; C++ and Java programming.</p>
+    </div>
+  </div>
+
+
+  <div class="line"></div>
+
+  <div id="services">
+    <h3>Research</h3>
+    <div class="service first">
+      <h4>My research domain is mainly discrete mathematics and theoretical computer science, centered on graph
+theory. <br />In particular:</h4>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>Structural graph theory.</h4>
+	<p></p>
+    </div>
+    <div class="service first"><img src="images/arrow.png" alt="" />
+      <h4>Algorithms and complexity.</h4>
+	<p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>Vertex and edge colorings, and vertex and edge colored graphs.</h4>
+	<p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>Hamiltonian cycles and paths.</h4>
+	<p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>Intersection graphs, clique graphs and biclique graphs.</h4>
+	<p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>Iterated graph operators.</h4>
+	<p></p>
+    </div>
+
+  </div>
+
+
+  <div class="line"></div>
+
+
+  <div id="services">
+    <h3>Publications</h3>
+	<div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>L. Montero: <b><a href="https://www.sciencedirect.com/science/article/abs/pii/S0166218X2200186X" target="_blank">"Vertex removal in biclique graphs"</a></b>, accepted in Discrete Applied Mathematics.</h4>
+    <p></p>
+	<div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>	L. Montero, S. Legay: <b><a href="https://onlinelibrary.wiley.com/doi/10.1002/jgt.22775" target="_blank">"On the edge-biclique graph and the iterated edge-biclique operator"</a></b>, Journal of Graph Theory. Vol 100(2): 232-251 (2022).</h4>
+    <p></p>
+	<div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>	M. Groshaus, L. Montero: <b><a href="https://ajc.maths.uq.edu.au/pdf/81/ajc_v81_p301.pdf" target="_blank">"Structural properties of biclique graphs and the distance formula"</a></b>, The Autralasian Journal of Combinatorics. Vol 81(2): 301-318 (2021).</h4>
+    <p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>	R. Agueda, N. Cohen, S. Fujita, S. Legay, Y. Manoussakis, Y. Matsui, L. Montero, R. Naserasr, H. Ono, Y. Otachi, T. Sakuma, Zs. Tuza, R. Xu: <b><a href="https://link.springer.com/article/10.1007/s10878-017-0205-2" target="_blank">"Safe sets in graphs: Graph classes and structural parameters"</a></b>, Journal of Combinatorial Optimization 36(4): 1221-1242 (2018).</h4>
+    <p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>	J.-A. Anglès d'Auriac, Cs. Bujtás, A. El Maftouhi, M. Karpinski, Y. Manoussakis, L. Montero, N. Narayanan, L. Rosaz, J. Thapper, Zs. Tuza: <b><a href="https://www.sciencedirect.com/science/article/abs/pii/S1570866718300595" target="_blank">"Tropical dominating sets in vertex-coloured graphs"</a></b>, Journal of Discrete Algorithms 48: 27-41 (2018).</h4>
+    <p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>R. Agueda, V. Borozan, R. Diaz, Y. Manoussakis, L. Montero: <b><a href="https://www.sciencedirect.com/science/article/abs/pii/S0012365X17300936" target="_blank">"Proper hamiltonian cycles in edge-colored multigraphs"</a></b>, Discrete Mathematics 340(8): 1897-1902 (2017).</h4>
+	<p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>R. Agueda, V. Borozan, Y. Manoussakis, L. Montero, M. Groshaus, G. Mendy: <b><a href="https://link.springer.com/article/10.1007/s00373-017-1803-6" target="_blank">"Proper Hamiltonian Paths in Edge-Coloured Multigraphs"</a></b>, Graphs and Combinatorics 33(4): 617-633 (2017).</h4>
+	<p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>M. Groshaus, A. Guedes, L. Montero: <b><a href="http://www.sciencedirect.com/science/article/pii/S0166218X15003753" target="_blank">"Almost every graph is divergent under the biclique operator"</a></b>, Discrete Applied Mathematics 201: 130-140 (2016).</h4>
+	<p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>M. Groshaus, L. Montero: <b><a href="http://www.sciencedirect.com/science/article/pii/S0304397516301633" target="_blank">"Tight lower bounds on the number of bicliques in false-twin-free graphs"</a></b>, Theoretical Computer Sciences 636: 77-84 (2016).</h4>
+	<p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>V. Borozan, L. Montero, N. Narayanan: <b><a href="http://ajc.maths.uq.edu.au/pdf/62/ajc_v62_p035.pdf" target="_blank">"Futher results on strong edge-colourings in outerplanar graphs"</a></b>, The Autralasian Journal of Combinatorics. Vol 62(1): 35-44 (2015).</h4>
+	<p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>M. Groshaus, L. Montero: <b><a href="http://onlinelibrary.wiley.com/doi/10.1002/jgt.21666/" target="_blank">"On the Iterated Biclique Operator"</a></b>. Journal of Graph Theory 73(2): 181-190 (2013).</h4>
+	<p></p>
+    </div>
+    <div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>V. Borozan, S. Fujita, A. Gerek, C. Magnant, Y. Manoussakis, L. Montero, Zs. Tuza: <b><a href="http://www.sciencedirect.com/science/article/pii/S0012365X11004079" target="_blank">"Proper connection of graphs"</a></b>. Discrete Mathematics 312(17): 2550-2560 (2012).</h4>
+	<p></p>
+    </div>
+  </div>
+  <div class="line"></div>
+
+<!--
+ <div id="services">
+    <h3>In progress</h3>    
+	<div class="service first"> <img src="images/arrow.png" alt="" />
+      <h4>L. Montero: <b><a href="https://arxiv.org/abs/2006.04583" target="_blank">"Vertex removal in biclique graphs"</a></b>, submitted.</h4>
+	<p></p>
+ </div>   
+ -->
+
+<!--
+  <div id="testimonials">
+    <h3>xxxxxxxxxxxxxx</h3>
+    <div class="testimonial">
+      <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquan voluptate velit esse cillum dolore eu fugiat nulla pariatur." </p>
+      <p class="author-details">Fred Jackson / <span>Web Media Solutions</span></p>
+    </div>
+    <div class="testimonial last">
+      <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquan voluptate velit esse cillum dolore eu fugiat nulla pariatur." </p>
+      <p class="author-details">Simon Smith / <span>Top Software Services</span></p>
+    </div>
+    <div class="testimonial">
+      <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquan voluptate velit esse cillum dolore eu fugiat nulla pariatur." </p>
+      <p class="author-details">Jessica Simpson / <span>Web Media Solutions</span></p>
+    </div>
+    <div class="testimonial last"> 
+      <p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliquan voluptate velit esse cillum dolore eu fugiat nulla pariatur." </p>
+      <p class="author-details">Sam Andrews / <span>Top Software Services</span></p>
+    </div>
+  </div>
+-->
+
+  <div id="footer">
+ <div id="photo-foot"> <img src="images/foto3.jpg" alt="" width="510" height="400" /> </div>
+    <p></p>
+  </div>
+
+  <div class="line"></div>  <div class="line"></div>
+
+</div>
+</body>
+</html>
